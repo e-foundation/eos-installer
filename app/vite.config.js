2ebeb3c29d108554cafe18a2b1f6f2bdf870f15a
Nicolas Gelot <nicolas.gelot@e.email>
2025-01-16 20:54:17 +0100

    Remove vendor from fastboot lib

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/@zip.js/zip.js/dist/z-worker-pako.js",
+          dest: "vendor",
+        },
+        {
+          src: "node_modules/pako/dist/pako_inflate.min.js",
+          dest: "vendor",
+        },
+      ],
+    }),
+  ],
 });
