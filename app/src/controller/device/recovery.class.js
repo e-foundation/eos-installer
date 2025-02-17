2d46ff33319a5a41c2dfa42b59e191f43d82db12
Nicolas Gelot <nicolas.gelot@e.email>
2025-02-17 11:36:33 +0100

    Fix sideload for A14 recovery
    Based on a tcpdump from adb client the MAX_PAYLOAD size sent was 0x10000
    `sideload-host-1686972035:65536`. So we patched accordingly the sideload
    with this value. The previous one was too big so it explains the
    failure we had.
    
    Ref: https://gitlab.e.foundation/e/devices/eos-installer/-/issues/1

--- app/src/controller/device/recovery.class.js
+++ app/src/controller/device/recovery.class.js
@@ -74,7 +74,7 @@ export class Recovery extends Device {
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
 
-    const MAX_PAYLOAD = 0x40000;
+    const MAX_PAYLOAD = 0x10000;
     const fileSize = blob.size;
     const service = `sideload-host:${fileSize}:${MAX_PAYLOAD}`; //sideload-host:1381604186:262144
 
